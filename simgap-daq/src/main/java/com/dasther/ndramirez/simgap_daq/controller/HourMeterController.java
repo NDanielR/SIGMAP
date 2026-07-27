519bc77ac0179500a1ff1e8aed94a61c9385e489
Nelson Daniel Ramirez <n.danielramirez.h@outlook.com>
2026-07-27 00:43:43 -0500

    Add class Controller

--- simgap-daq/src/main/java/com/dasther/ndramirez/simgap_daq/controller/HourMeterController.java
+++ simgap-daq/src/main/java/com/dasther/ndramirez/simgap_daq/controller/HourMeterController.java
@@ -1,5 +1,29 @@
 package com.dasther.ndramirez.simgap_daq.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dasther.ndramirez.simgap_daq.model.dto.HourMeterResponseDto;
+import com.dasther.ndramirez.simgap_daq.service.HourMeterService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("api/v1/horometro")
 public class HourMeterController {
+
+    private final HourMeterService hourMeterService;
+
+    public HourMeterController (HourMeterService hourMeterService){
+        this.hourMeterService = hourMeterService;
+    }
+    
+    @GetMapping
+    public List<HourMeterResponseDto> getAllHourMeters() {
+        return hourMeterService.getAll();
+    }
     
 }
