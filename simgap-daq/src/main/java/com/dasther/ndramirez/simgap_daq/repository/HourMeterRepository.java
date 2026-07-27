45183eaf6b744b1f592ada295d852913b49675b5
Nelson Daniel Ramirez <n.danielramirez.h@outlook.com>
2026-07-27 00:42:50 -0500

    Add class Repository JPA

--- simgap-daq/src/main/java/com/dasther/ndramirez/simgap_daq/repository/HourMeterRepository.java
+++ simgap-daq/src/main/java/com/dasther/ndramirez/simgap_daq/repository/HourMeterRepository.java
@@ -2,7 +2,7 @@ package com.dasther.ndramirez.simgap_daq.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.dasther.ndramirez.simgap_daq.model.HourMeters;
+import com.dasther.ndramirez.simgap_daq.model.entity.HourMeters;
 
 public interface HourMeterRepository extends JpaRepository <HourMeters,Long> {
     
